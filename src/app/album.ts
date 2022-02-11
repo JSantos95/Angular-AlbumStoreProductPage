436b3160b240e300ecc687a35dbce7976415a833
Jose <santosjosel1995@gmail.com>
2022-02-11 12:04:34 -0500

    update(): Part 3

---
+++ src/app/album.ts
@@ -0,0 +1,19 @@
+export interface Track {
+    trackNumber: number;
+    trackName: string;
+    trackLength: string;
+    trackPrice: number;
+}
+
+export interface Album {
+    name: string,
+    releaseDate: string,
+    coverImage: string,
+    tracks: Track[],
+}
+
+export interface CD {
+    id: number,
+    artist: string,
+    album: Album,
+}
---
+++ src/app/product.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+
+import 'rxjs/add/operator/map';
+
+import { Album, CD } from './album';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl = '../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number): Observable<CD> {
+    return this._http.get(this._albumUrl).map((response) => <CD>response.json())
+  }
+
+}
